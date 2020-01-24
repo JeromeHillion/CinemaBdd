ba8cefc8c1103dcb129c9adfa5bcef82698d9a76
GIDEON <jerome.gideon29@gmail.com>
2020-01-24 19:46:54 +0100

    nettoyage du code

--- CinemaBdd/connexion.php
+++ CinemaBdd/connexion.php
@@ -5,36 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <title>Connexion</title>
 </head>
 <body>
-<div class="field">
-    <p class="control has-icons-left has-icons-right">
-        <input class="input" type="email" placeholder="Email">
-        <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-        <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-    </p>
-</div>
-<div class="field">
-    <p class="control has-icons-left">
-        <input class="input" type="password" placeholder="Password">
-        <span class="icon is-small is-left">
-      <i class="fas fa-lock"></i>
-    </span>
-    </p>
-</div>
-<div class="field">
-    <p class="control">
-        <button class="button is-success">
-            Login
-        </button>
-    </p>
-</div>
+
 </body>
 </html>
--- CinemaBdd/index.php
+++ CinemaBdd/index.php
@@ -4,60 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello Gidéon!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
-<div class="container">
-    <section class="section">
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                </a>
 
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                   data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+<div id="filmsSection"></div>
 
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item">
-                        Films
-                    </a>
-
-                    <a class="navbar-item">
-                        Séries Tv
-                    </a>
-                    <a class="navbar-item">
-                        Animations
-                    </a>
-
-
-                </div>
-
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary" href="inscription.php">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a class="button is-light" href="connexion.php">
-                                Log in
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </section>
-
-    <div id="filmsSection"></div>
-</div>
 </body>
 <script src="reqFilms.js"></script>
 
