020a34b9ebfa1445fa0ddd82d37565d80da3f0de
GIDEON <jerome.gideon29@gmail.com>
2020-01-24 19:52:24 +0100

    nettoyage du code

--- CinemaBdd/index.php
+++ CinemaBdd/index.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Gidéon!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello Gidéon !</title>
 </head>
 <body>
-
 <div id="filmsSection"></div>
-
 </body>
-<script src="reqFilms.js"></script>
-
 </html>
